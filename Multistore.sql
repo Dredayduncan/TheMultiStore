e6df2556a4c7c7a042921113470ebc6ca2467bf4
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2021-04-01 00:43:31 +0000

    Edits to database

--- Multistore.sql
+++ Multistore.sql
@@ -6,16 +6,20 @@ create table Users(
     lastName varchar(50),
 	username varchar(50) unique,
     primary key(username),
-    password varchar(60),
+    password varchar(255),
     email varchar(255),
     DOB date
-    
-    
 );
 
 create table SearchHistory(
 	username varchar(50),
 	foreign key(username) references Users(username),
-    searchHistoryTyped varchar(50),
-    searchHistoryLink varchar(255)
+    searchHistory varchar(255)
+);
+
+create table History(
+	username varchar(50),
+    foreign key(username) references Users(username),
+    HistoryName varchar(50),
+    HistoryLink varchar(255)
 );
