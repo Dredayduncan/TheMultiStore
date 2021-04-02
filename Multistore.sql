ecc55e94be33f19eb5f8154ebb5b3961c7b159f8
kwakuAgyemang <59964154+kwakuAgyemang@users.noreply.github.com>
2021-04-02 02:52:20 +0000

    Updates to the database

--- Multistore.sql
+++ Multistore.sql
@@ -1,14 +1,19 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+drop database if exists Multistore;
 create database Multistore;
 use Multistore;
 
 create table Users(
-	firstName varchar(20),
-    lastName varchar(50),
+	firstName varchar(20) NOT NULL,
+    lastName varchar(50) NOT NULL,
 	username varchar(50) unique,
     primary key(username),
-    password varchar(255),
-    email varchar(255),
-    DOB date
+    password varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    DOB date NOT NULL
 );
 
 create table SearchHistory(
@@ -20,6 +25,10 @@ create table SearchHistory(
 create table History(
 	username varchar(50),
     foreign key(username) references Users(username),
-    HistoryName varchar(50),
-    HistoryLink varchar(255)
+    HistoryName varchar(50) NOT NULL,
+    ImageSrc varchar(255),
+    HistoryLink varchar(255) NOT NULL,
+    price float,
+    rating float,
+    CHECK(rating <= 10.0)
 );
